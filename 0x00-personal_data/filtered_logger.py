4c4e0358c2672fbabe1c684af406a5894846f1f3
lourdel <klourdel@gmail.com>
2023-05-28 05:48:57 +0300

    modified filter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,3 +31,59 @@ class RedactingFormatter(logging.Formatter):
         """ format """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """Function that creates a logger object named
+    user_data, logs upto INFO, contains a streamhandler
+    that includes same RedactingFormatter"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    formatter = RedactingFormatter(fields=list(PII_FIELDS))
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Function that creates the mysql connector using given
+    credentials
+    Returns:
+        MySQLConnection object to a mysql database
+    """
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    mydb = mysql.connector.connect(
+        host=host,
+        user=username,
+        password=password,
+        database=db_name
+    )
+    return mydb
+
+
+def main():
+    """
+    Function obtains a database connection, retrieves all rows
+    under a filtered format like this
+    """
+    logger = get_logger()
+    connection = get_db()
+    query = f'SELECT * FROM users;'
+
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        col_names = [i[0] for i in cursor.description]
+        for row in rows:
+            record = map(lambda x: f'{x[0]}={x[1]}', zip(col_names, row))
+            message = f"{'; '.join(list(record))};"
+            args = ("user_data", logging.INFO, None, None, message, None, None)
+            log_record = logging.LogRecord(*args)
+            logger.handle(log_record)
