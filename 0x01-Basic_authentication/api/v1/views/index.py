711011806cfcd3c80f180501604aad839f47a281
lourdel <klourdel@gmail.com>
2023-05-30 22:30:22 +0300

    unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def status_unauth() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - the auth status of the API
+    """
+    abort(401)
