65c488d23ead26394a8d647cee189df9c4a1fe01
lourdel <klourdel@gmail.com>
2023-06-05 16:04:07 +0300

    session auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -19,6 +19,9 @@ excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
 authentic = getenv("AUTH_TYPE")
 
 if authentic:
+    if authentic == "session_auth":
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
     if authentic == "basic_auth":
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
@@ -40,6 +43,20 @@ def filter():
         request.current_user = auth.current_user(request)
         if request.current_user is None:
             abort(403)
+
+
+def authenticate_user():
+    """Authenticates a user before processing a request.
+    """
+    if auth:
+        if auth.require_auth(request.path, excluded_paths):
+            user = auth.current_user(request)
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
+                abort(401)
+            if user is None:
+                abort(403)
+            request.current_user = user
 
 
 @app.errorhandler(404)
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""class session auth"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class implements session_auth"""
