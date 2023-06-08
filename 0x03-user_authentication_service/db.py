2d4e7985117f62cbcfecbe6a173b151674435bf4
lourdel <klourdel@gmail.com>
2023-06-08 14:36:21 +0300

    find user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,8 @@ from sqlalchemy.orm.session import Session
 
 from user import Base
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 class DB:
@@ -37,3 +39,13 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Method returns the first row found in the users table"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found")
+            return user
+        except InvalidRequestError as e:
+            raise InvalidRequestError("Invalid Query: {}".format(e)) from e
