0629e27b067a5d988b5344ea080320b4a2c380ff
lourdel <klourdel@gmail.com>
2023-06-08 16:16:27 +0300

    register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """_hash_password method"""
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,22 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pwd = bcrypt.hashpw(password, salt)
     return hashed_pwd
+
+
+class Auth():
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method registers the user to the DB"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            user = self._db.add_user(email=email, hashed_password=hashed_pwd)
+            return user
+        else:
+            raise ValueError('User {} already exists'.format(email))
