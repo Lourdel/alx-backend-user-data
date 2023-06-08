956473d56a6169ca3dc39d514c0165668fc83b59
lourdel <klourdel@gmail.com>
2023-06-08 22:18:35 +0300

    implemented login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,5 +28,19 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """login function"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        res = make_response({"email": email, "message": "logged in"})
+        res.set_cookie('session_id', session_id)
+        return res, 200
+    return abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
