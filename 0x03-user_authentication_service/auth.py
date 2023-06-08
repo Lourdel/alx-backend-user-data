d54de1a2c2ff3429c126bf77db43b6b0f4eef9d4
lourdel <klourdel@gmail.com>
2023-06-09 00:13:09 +0300

    uploaded files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,28 @@ class Auth():
         if user_id is not None:
             self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Finds user corresponding to the email & generates
+        a UUID for the reset token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """update user password
+        with new hashed
+        password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_passwrd = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=hashed_passwrd,
+                                 reset_token=None)
+            return None
+        except NoResultFound:
+            raise ValueError
